23a52bea04cac125683d2acb8db830df77eb74f1
Nhat Rocky Nguyen <nhatrockynguyen@gmail.com>
2017-02-21 21:43:06 -0800

    Comments updated.

--- Project1a/main.cpp
+++ Project1a/main.cpp
@@ -1,5 +1,5 @@
 // Assignment 1: Alternating disks problem, left-to-right algorithm   
-// Justin Shelley
+// Justin Shelley & Rocky N. Nguyen
 // Given 2n alternating disks (dark, light) the program reads the number of single color disks 
 // (light or dark), arranges the disks in the correct order and outputs the number of swaps 
 // INPUT: a positive integer n and a list of 2n disks of alternating colors dark-light, starting with dark 
@@ -11,7 +11,6 @@
 #include <cstdlib>
 using namespace std;
 void print_disks(int n, char *disks);
-// YOU NEED TO IMPLEMENT THIS FUNCTION
 // function to print the list of disks, given the number of single color disks and the actual list  
 // n represents the number of single color disks 
 // disks represents the list of disks (index 0 being the first disk) where
@@ -36,10 +35,10 @@ int main() {
     // print the initial configuration of the list of disks
     cout << "Initial configuration" << endl;
     print_disks(n,disks);
-// PART OF CODE MISSING
+    
     // loop to push light one before the darks ones
     for (k=0;  k < n ; k++) {
-        // YOU NEED TO COMPLETE THIS PART OF CODE FOR GOING LEFT TO RIGHT
+        // CODE FOR GOING LEFT TO RIGHT
         for (i = k; i < 2*n - 1; i +=2) {
             if (disks[i] > disks[i+1]) {
                 char temp = disks[i];
